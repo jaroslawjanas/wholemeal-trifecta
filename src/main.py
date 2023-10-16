1d5d42c601ad1d4f718a12fdb30331e95b36dbae
jaroslawjanas <jaroslawjanas.dev@gmail.com>
2023-10-16 17:22:40 +0200

    Implemented file open window

--- src/main.py
+++ src/main.py
@@ -1,5 +1,14 @@
+import tkinter as tk
+from tkinter import filedialog
+
+
 def main():
-    print("Hello World!")
+    # Hide the root dialog window
+    root = tk.Tk()
+    root.withdraw()
+
+    # Ask for file to open
+    file_path = filedialog.askopenfilename()
 
 
 if __name__ == '__main__':
